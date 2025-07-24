9e9ff6f8d85e0e8d1426d40f9be3074e2f854b12
Stephens6623 <stephenspargo134@gmail.com>
2025-07-24 00:09:41 -0400

    added routes to index.js fixed hiding .env and node modules

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,6 +1,6 @@
 // User controller
 const User = require("../schemas/user.model");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 // signup new user
--- server/index.js
+++ server/index.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 const PORT = process.env.PORT || 5000;
+const userRoutes = require('./routes/user-routes');
+const messageRoutes = require('./routes/message-routes');
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((error) => {
+  console.error('MongoDB connection error:', error);
+});
+
 // Create an Express application
 const app = express();
 //Middleware
@@ -9,7 +27,11 @@ app.use(express.json());
 app.get('/api/health', (req, res) => {
   res.status(200).json({ message: 'Server is running' });
 });
-
+// User routes
+app.use('/api/users', userRoutes);
+// Message routes
+app.use('/api/messages', messageRoutes);
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
